3873a9f6e99b2ea6b7ed5740d99b953a7c01dc6e
artchess <art_chess@hotmail.com>
2016-05-31 20:35:27 -0500

    88

---
+++ BootstrapExample/Scripts/ViewModels/AutorIndexViewModel.js
@@ -0,0 +1,22 @@
+﻿
+function AutorIndexViewModel(autores) {
+    var self = this;
+    self.autores = autores;
+
+    self.muestraModalEliminacion = function (data, event) { // data contiene el autor, event contiene el elemento html al que el evento click es atachado
+        self.enviando = ko.observable(false);
+
+        $.get($(event.target).attr('href'), function (d) {
+            $('.body-content').prepend(d); // agrego el html al body content del modal.
+            $('#modalEliminacion').modal('show');
+
+            ko.applyBindings(self, document.getElementById('modalEliminacion'));
+        });
+    };
+
+    self.eliminaAutor = function (form) {
+        debugger;
+        self.enviando(true);
+        return true;
+    };
+};
