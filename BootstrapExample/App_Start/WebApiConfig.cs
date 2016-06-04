977aaac7f74171714700ba03e7cb645255848d83
artchess <art_chess@hotmail.com>
2016-06-03 19:44:34 -0500

    102

---
+++ BootstrapExample/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BootstrapExample
+{
+    // Esta clase es como el RouteConfig de MVC pero este es para Web Api
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuraciones y servicios de Web Api
+
+            // rutas de web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
